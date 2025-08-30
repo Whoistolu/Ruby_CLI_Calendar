0a5342ff35f4d87a47c7a4e34c85b107bb5e4fba
Whoistolu <digittolu25@gmail.com>
2025-08-31 00:22:41 +0100

    `Add initialize method to Calendar class with instance variables for today's date.`

--- cal.rb
+++ cal.rb
@@ -1,4 +1,10 @@
 require 'date'
 
 class Calendar
+    def initialize
+        @today = Date.today
+        @year = @today.year
+        @month = @today.month
+        @day = @today.day
+    end
 end
