93fcb2fb8d2658e5fdd4625b9b9be9b4291e4ec4
Whoistolu <digittolu25@gmail.com>
2025-09-01 23:54:45 +0100

    `Add new test case for Calendar#generate method`

--- spec/cal_spec.rb
+++ spec/cal_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe Calendar do
     expect(output).to include(Date::MONTHNAMES[today.month])
     expect(output).to include(today.year.to_s)
   end
+
+  it "prints the given month and year" do
+    output = calendar.generate(8, 2025)
+    expect(output).to include("August 2025")
+    expect(output).to include("31")
+  end
 end
