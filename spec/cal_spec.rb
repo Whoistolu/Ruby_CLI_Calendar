32bd907d12c00b12fc2c83d25033afee7ffdff50
Whoistolu <digittolu25@gmail.com>
2025-09-01 23:55:26 +0100

    `Add test for aligning first day of month under correct weekday`

--- spec/cal_spec.rb
+++ spec/cal_spec.rb
@@ -15,4 +15,11 @@ RSpec.describe Calendar do
     expect(output).to include("August 2025")
     expect(output).to include("31")
   end
+
+  it "aligns the first day of the month under the correct weekday" do
+    output = calendar.generate(9, 2025)
+    lines = output.split("\n")
+    week_line = lines[2]
+    expect(week_line.start_with?("   ")).to be true
+  end
 end
