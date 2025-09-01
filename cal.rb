ca2f1668eb6bd8e875b286cbcc006c42d63d090c
Whoistolu <digittolu25@gmail.com>
2025-09-02 00:01:21 +0100

    Create a script to make it interactive

---
+++ cal.rb
@@ -0,0 +1,19 @@
+require_relative "lib/calendar"
+
+calendar = Calendar.new
+
+begin
+  case ARGV.size
+  when 0
+    puts calendar.generate
+  when 1
+    puts calendar.generate(Date.today.month, ARGV[0].to_i)
+  when 2
+    puts calendar.generate(ARGV[0].to_i, ARGV[1].to_i)
+  else
+    puts "Usage: ruby cal.rb [month] [year]"
+  end
+rescue ArgumentError => e
+  puts "Error: #{e.message}"
+  puts "Usage: ruby cal.rb [month] [year]"
+end
