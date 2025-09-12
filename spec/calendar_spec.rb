4e180838868ee92795ae0122eefb804a5b0a3e07
Whoistolu <digittolu25@gmail.com>
2025-09-12 23:19:46 +0100

    `Add test for days_in_month method in Calendar class`

--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -7,4 +7,11 @@ Rspec.describe Calendar do
       expect(Calendar.header(8, 2025)).to eq("     August 2025")
     end
   end
+
+  describe ".days_in_month" do
+    it "returns correct number of days including leap years" do
+      expect(Calendar.days_in_month(2, 2024)).to eq(29)
+      expect(Calendar.days_in_month(2, 2025)).to eq(28)
+    end
+  end
 end
