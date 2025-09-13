6dbdf32f081b5b6677890a2a762949341288fa88
Whoistolu <digittolu25@gmail.com>
2025-09-13 23:07:15 +0100

    "Remove two test assertions in Calendar weekday_offset spec"

--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -17,10 +17,7 @@ Rspec.describe Calendar do
 
   describe ".weekday_offset" do
     it "returns 0 for Sunday, 1 for Monday, etc. for the first day of the month" do
-      # Sept 1, 2025 is a Monday → offset should be 1
       expect(Calendar.weekday_offset(9, 2025)).to eq(1)
-
-      # Aug 1, 2025 is a Friday → offset should be 5
       expect(Calendar.weekday_offset(8, 2025)).to eq(5)
     end
   end
