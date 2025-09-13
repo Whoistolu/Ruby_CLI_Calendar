291fc30ca86252bfff750883548c36be59394c5c
Whoistolu <digittolu25@gmail.com>
2025-09-13 23:04:49 +0100

    `Add weekday_offset method to Calendar class in calendar_spec.rb`

--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -14,4 +14,14 @@ Rspec.describe Calendar do
       expect(Calendar.days_in_month(2, 2025)).to eq(28)
     end
   end
+
+  describe ".weekday_offset" do
+    it "returns 0 for Sunday, 1 for Monday, etc. for the first day of the month" do
+      # Sept 1, 2025 is a Monday → offset should be 1
+      expect(Calendar.weekday_offset(9, 2025)).to eq(1)
+
+      # Aug 1, 2025 is a Friday → offset should be 5
+      expect(Calendar.weekday_offset(8, 2025)).to eq(5)
+    end
+  end
 end
