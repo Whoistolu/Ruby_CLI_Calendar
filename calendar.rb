62185e7264de201d83636e3e9e22dde85fb72b2d
Whoistolu <digittolu25@gmail.com>
2025-09-18 00:00:19 +0100

    "Add methods `header` and `days_in_month` to the `Calendar` class."

--- calendar.rb
+++ calendar.rb
@@ -4,4 +4,8 @@ class Calendar
     def self.header(month, year)
         "     #{Date::MONTHNAMES[month]} #{year}"
     end
+
+    def self.days_in_month(month, year)
+        Date.new(year, month, -1).day
+    end
 end
