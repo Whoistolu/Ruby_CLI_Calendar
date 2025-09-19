04e7bbe54a5e8aae8a58134bc400be6308ec3526
Whoistolu <digittolu25@gmail.com>
2025-09-20 00:00:16 +0100

    Add show_calendar method to the interactive_cal.rb file

---
+++ interactive_cal.rb
@@ -0,0 +1,18 @@
+require '../calendar'
+
+require "date"
+
+def show_calendar(month, year)
+  puts Calendar.header(month, year)
+  puts "Su Mo Tu We Th Fr Sa"
+
+  first_day = Date.new(year, month, 1)
+  last_day  = Date.new(year, month, -1)
+
+  print "   " * first_day.wday
+  (1..last_day.day).each do |day|
+    print day.to_s.rjust(2) + " "
+    print "\n" if Date.new(year, month, day).saturday?
+  end
+  puts "\n"
+end
