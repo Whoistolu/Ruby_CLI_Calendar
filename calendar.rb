5a5af9eb27fb96ae1e7485259fe38f4031679454
Whoistolu <digittolu25@gmail.com>
2025-10-08 23:59:33 +0100

    `Add calendar layout and day printing functionality`

--- calendar.rb
+++ calendar.rb
@@ -10,3 +10,15 @@ last_day  = Date.new(year, month, -1)
 
 puts "     #{Date::MONTHNAMES[month]} #{year}"
 puts "Su Mo Tu We Th Fr Sa"
+
+print "   " * first_day.wday
+
+(1..last_day.day).each do |day|
+  print day.to_s.rjust(2)
+  print " "
+  if Date.new(year, month, day).saturday?
+    print "\n"
+  end
+end
+
+print "\n"
